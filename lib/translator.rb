533dfe572b398ed2e8fbe05e8b2fccb4712cca5b
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-16 19:47:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   emoticon_hash = {}
   emoticons.each do |name, value|
-    emoticon_hash << {name => {:english => value[0], :japanese => value[1]}}
+    emoticon_hash = {name => {:english => value[0], :japanese => value[1]}}
   end
 end
 
